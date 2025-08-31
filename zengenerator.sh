ca68204f12038b9af37c7885c02235b05ebe0119
Alex T <lexkie@proton.me>
2025-08-31 17:56:42 -0400

    Add files via upload

---
+++ zengenerator.sh
@@ -0,0 +1,18 @@
+
+
+echo "Press 'q' to exit zen generator."
+while true; do
+minmax=$(( (RANDOM % 9) + 0 ))
+bamboo=$(find /bamboo -type f | shuf -n 1)
+sleepdur="0."
+sleepdur+=$minmax
+play -q "$bamboo" vol "$sleepdur" &
+sleep $sleepdur
+sleepdur=""
+read -n 1 -t 0.1 input
+    if [[ $input = "q" ]] || [[ $input = "Q" ]]; then
+        echo "Exiting loop."
+        pkill play
+        break # Exit the loop if 'q' is pressed
+    fi
+done
